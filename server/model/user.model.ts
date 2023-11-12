cb78934ccb7853ee01eed0a18d0cad31cbe7cc6e
wangjianing <jianingmica@gmail.com>
2023-11-12 18:11:24 +0100

    feat: get user choice when render, client

--- server/model/user.model.ts
+++ server/model/user.model.ts
@@ -2,16 +2,28 @@ import moment from 'moment';
 import { UserModel } from './index';
 
 const getUser = async (id: string) => {
-  const todos = await UserModel.findOne({ _id: id }).populate({
-    path: 'todo',
-    select: 'content status',
-    match: {
-      date: {
-        $gte: moment().startOf('date'),
-        $lte: moment().endOf('date'),
+  const todos = await UserModel.findOne({ _id: id })
+    .populate({
+      path: 'todo',
+      select: 'content status',
+      match: {
+        date: {
+          $gte: moment().startOf('date'),
+          $lte: moment().endOf('date'),
+        },
       },
-    },
-  });
+    })
+    .populate({
+      path: 'choice',
+      select: 'date type',
+      match: {
+        date: {
+          $gte: moment().startOf('date'),
+          $lte: moment().endOf('date'),
+        },
+      },
+    })
+    .exec();
 
   console.log(todos);
 
