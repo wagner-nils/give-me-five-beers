5f19bb9391de4d37e084db2c0faa18c8a3c8937b
wangjianing <jianingmica@gmail.com>
2023-11-14 10:52:00 +0100

    style: affirmation ux

--- client/src/components/AffirmationBox.tsx
+++ client/src/components/AffirmationBox.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import classNames from 'classnames';
+
 import Text from './Text';
 
 import '../styles/Affirmation.css';
@@ -9,20 +12,28 @@ type Props = {
 };
 const AffirmationBox = ({ display, setDisplay, setSeen }: Props) => {
   // todo: define different types of affirmation
+  const [clicked, setClicked] = useState(false);
 
   if (!display) {
     return null;
   }
 
   const handleClick = () => {
-    setDisplay(false);
-    setSeen(true);
+    setClicked(true);
+    setTimeout(() => {
+      setDisplay(false);
+      setSeen(true);
+    }, 2000);
   };
+
+  const btnClassnames = classNames('affirmation-btn', {
+    clicked,
+  });
 
   return (
     <div className="affirmation-box">
       <Text text="You are the best!" large />
-      <button className="affirmation-btn" onClick={() => handleClick()}>
+      <button className={btnClassnames} onClick={() => handleClick()}>
         I KNOW
       </button>
     </div>
