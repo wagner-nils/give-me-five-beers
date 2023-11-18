a8cf87342c106dd382d335021fc91c7b3ea1e143
Sally Carlund <sally.carlund@gmail.com>
2023-11-18 18:09:30 +0100

    working endpoint tests for bar controllers

--- server/test/bar-controller.test.ts
+++ server/test/bar-controller.test.ts
@@ -1,78 +1,41 @@
 // TODO Happy route: tests for expected input
 import request from 'supertest'
-import express from 'express'
-import assert from 'assert'
 import app from '../index'
-// const request = require('supertest');
-// const express = require('express');
-// const router = require('../router')
-// var request = require('supertest')
-// var express = require('express')
 
+import mongoose from 'mongoose'
 
-describe ('GET /', () => {
-    it('responds with message', async () => {
-        return request(app)
-        .get('/')
-        // .set('Accept', 'application/json')
-        // .expect('Content-Type', /json/)
-        .expect(400)
-        .then(response => {
-            console.log(response.body)
-            assert(response.body, 'hello from router, i love typescript')
-        })
+let con: any;
 
-
-
-        // const res = request(app)
-        // res.get('http://localhost:3000/')
-        // .expect(res.body.message).toBe('hello from router, i love typescript')
-        
-        // // .get('http://localhost:3000/')
-        // .set('Accept', 'text')
-        // .expect(200)
-        // .end(function(err, res) {
-        //   if (err) throw err;
-        // });
-    })
-})
-
-
-
-
-
-
-
-
-
-
-// const app = express()
-
-/* // Connects to LOCAL database called avengers
 beforeAll(async () => {
-    const app = express()
-    request(app)
-    // const url = `mongodb://127.0.0.1/avengers`
-    // console.log(request(app))
-    // await mongoose.connect(url, { useNewUrlParser: true })
-  })
+    await mongoose.connect(process.env.MONGO_URI!);
+    con = await request(app)
+  });
 
 
-describe ('GET /', () => {
-    it('responds with message', async () => {
-        const res = request(app)
-        res.get('http://localhost:3000/')
-        console.log(res)
-        // res
-        // .get('http://localhost:3000/')
-        // .set('Accept', 'text')
-        // .expect(response.body.message).toBe('hello from router, i love typescript')
-        // .expect(200)
-        // .end(function(err, res) {
-        //   if (err) throw err;
-        // });
+describe ('Endpoint /choice', () => {
+    it('Gets a random bar', async () => {
+        const res = await con.get('/choice/bar')
+        // console.log('res')
+        expect(res.status).toBe(200)
     })
+    it('Gets a chosen bar', async () => {
+        // const con = await request(app)
+        const res = await con.get('/choice/bar/6556300d1774242995441f70')
+        // console.log('TEXT:', res.text)
+        expect(res.status).toBe(200)
+    })
+    // THIS WORKS, JUST HAVE TO ADD ERROR HANDLING IN BAR.CONTROLLER
+    // it('Throws an error when ID does not exist', async () => {
+    //     // const con = await request(app)
+    //     const res = await con.get('/choice/bar/6556300d1774985793485739847')
+    //     console.log('TEXT:', res.status)
+    //     expect(res.status).not.toBe(200)
+    // })
 })
 
 
-// TODO Unhappy route: tests for unexpected input */
+
+
+afterAll(() => {
+    mongoose.disconnect()
+})
