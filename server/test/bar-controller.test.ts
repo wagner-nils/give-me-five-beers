d5d366b80b026b0d4ae424c0335a45d821dcf5ad
Sally Carlund <sally.carlund@gmail.com>
2023-11-20 12:17:43 +0100

    added mocks

--- server/test/bar-controller.test.ts
+++ server/test/bar-controller.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import app from '../index';
 import mongoose from 'mongoose';
+import * as BarModel from '../model/bar.model';
 
 let con: any;
 
@@ -11,19 +12,37 @@ beforeAll(async () => {
 
 
 describe('getRandomBar controller', () => {
+    const bar = {
+        _id: new ObjectId('6556300d1774242995441f70'),
+        formattedAddress: 'C/ de Còrsega, 611, 08025 Barcelona, Spain',
+        name: 'La Cerveseria Clandestina',
+        placeId: 'ChIJD1IZ2tqipBIRoUD-UMvz4nw',
+        url: 'https://maps.google.com/?cid=8999023060002422945',
+        website: 'https://m.facebook.com/LaCerveseriaClandestina/?locale2=ca_ES',
+        wheelchairAccessibleEntrance: true,
+        city: 'Barcelona',
+        country: 'spain',
+        __v: 0
+      };
+
+    (BarModel.getRandomBar as jest.Mock).mockResolvedValue(bar);
+
     it('Responds with status code 200', async () => {
         const res = await con.get('/choice/bar');
         expect(res.status).toBe(200);
-    })
+    }, 70000);
 
-
-})
+    it('Responds with the bar', async () => {
+        const res = await con.get('/choice/bar');
+        expect(res.text).toBe(bar)
+    }, 70000);
+});
 
 describe('getChosenBar controller', () => {
     it('Responds with status code 200', async () => {
         const res = await con.get('/choice/bar/6556300d1774242995441f70');
         expect(res.status).toBe(200);
-    })
+    }, 70000);
 
 
     // THIS WORKS, JUST HAVE TO ADD ERROR HANDLING IN BAR.CONTROLLER
@@ -33,7 +52,7 @@ describe('getChosenBar controller', () => {
     //     console.log('TEXT:', res.status)
     //     expect(res.status).not.toBe(200)
     // })
-})
+});
 
 afterAll(() => {
     mongoose.disconnect()
